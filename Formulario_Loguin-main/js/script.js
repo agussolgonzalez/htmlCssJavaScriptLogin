a603615dd92e667807d6bfc57c551dc84b39f091
Clara Depiante Corchero <101030637+xlara7@users.noreply.github.com>
2022-11-13 15:15:08 -0300

    Update script.js

--- Formulario_Loguin-main/js/script.js
+++ Formulario_Loguin-main/js/script.js
@@ -1,10 +1,12 @@
 window.addEventListener("resize", anchoPagina);
 
 function anchoPagina(){
+    if (window.innerWidth > 850){
+        form - login.style.display; "block";
      container.style.display= "block";
     }else{
-     form-login.style.display= "block";
-     form-login.style.opacity = "1";
+        form - login.style.display; "block";
+        form - login.style.opacity; "1";
      container.style.left= "0";
     }
 
